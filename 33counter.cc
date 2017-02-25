3a0a8ced82f5517fe0805431592db8684eb51049
Fernanda Castano <lfcj@Fernandas-MBP.fritz.box>
2017-02-25 22:53:02 +0100

    clean code, add author

--- 33counter.cc
+++ 33counter.cc
@@ -2,7 +2,10 @@
 #include <string>
 #include <cmath>
 
-
+/*
+        Book: Accelerated C++, Koenig & Moo.
+        @author: Luisa Castano, fernanda16400@gmail.com
+*/
 /** PROBLEM:
   Write a program to count how many times each distinct
   word appears in its input. **/
@@ -29,18 +32,12 @@ Result would be:
 using namespace std;
 // ascii A (65) to z (122)
 
-int max (int a, int b){
-  return ((a+b)-abs(a+b));
-}
-
 bool next_is_stop(int index, int* stops, int stops_len){
-  // cout << "index> " << index << endl;
-  // cout << "stops_len: " << stops_len << endl;
+  // cout << "index> " : " << stops_len << endl;
   bool res = true;
 
   for (int i=0;i<stops_len;i++){
     res = stops[i] == index;
-    // cout << "stop: " << stops[i] << endl;;
     if (res)
       break;
   }
@@ -59,23 +56,14 @@ int compare_words(char* text, int begin, int end, int* stops, int text_len, int 
   while ((current_begin + word_len) <= text_len +1){
     equal = true;
     current_end = current_begin + word_len;
-    // cout << "current end: " << current_begin+word_len << endl;
-    // cout << "word_len " << word_len << endl;
     for (int i = current_begin; i < current_end && equal;i++){
-      // cout << "current " << current_begin << endl;
-      // cout << "iterator> " << iterator << endl;
       equal = equal && (text[i] == text[iterator]);
       iterator++;
     }
-    // cout << "equal: " << equal << endl;
-    // found word
-    // || current_end == text_len)
     if (equal && next_is_stop(current_end, stops, stops_len)){
       count++;
       cout << count << endl;
 
-      // "delete" word to avoid counting it again.
-      // cout << current_begin << " cu " << current_end << endl;
       for (int i=current_begin;i<current_end;i++)
         text[i]='^';
     }
@@ -128,14 +116,10 @@ int main () {
       cout << "' is " << current_count << " times in text." << endl;
     }
   }
-  cout << endl;
-  for (int i=0;i<=input_len;i++)
-    cout << input[i];
-  cout << endl;
-  // cout << counter << ", word is:" << input[0] << input[1] << input[2] << input[3]<< endl;
-  // cout << (-1 % 1) << endl;
-  // cout << endl << stops_iterator <<  endl;
-  // cout  << input_len <<  endl;
+  // cout << endl;
+  // for (int i=0;i<=input_len;i++)
+  //   cout << input[i];
+  // cout << endl;
 
   return 0;
 }
