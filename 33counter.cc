5e81edfe963e331f41e8cd81a394df2f73ccf0c0
lfcj <fernanda16400@gmail.com>
2017-03-07 21:53:45 +0100

    Extract global functions to .h file

--- 33counter.cc
+++ 33counter.cc
@@ -1,11 +1,6 @@
 #include "accelerated.h"
 
-#define SIZE 100
-
-char input[SIZE];
-int stops[SIZE/2];
 /*
-        Book: Accelerated C++, Koenig & Moo.
         @author: Luisa Castano, fernanda16400@gmail.com
 */
 /** PROBLEM:
@@ -31,26 +26,7 @@ Result would be:
 'n'    is 1 time in text
 'o'    is 1 time in text
 ***********************/
-using namespace std;
 
-bool next_is_stop(int index, int stops_len){
-  bool res = true;
-
-  for (int i=0;i<stops_len;i++){
-    res = stops[i] == index;
-    if (res)
-      break;
-  }
-  return res;
-}
-/* returns the amount of times that word is repeated in text
- *
- * begin: index of char where word being compared starts with.
- * end:   index of char where word being compared ends + 1.
- * text_len: length of user input.
- * stops_len: amount of white spaces, equal amout of words.
- * index: current stop index.
- */
 int compare_words(int begin, int end, int text_len, int stops_len, int index){
   int stop_iterator = index;
   int current_begin = stops[stop_iterator] + 1;
@@ -67,7 +43,7 @@ int compare_words(int begin, int end, int text_len, int stops_len, int index){
       equal = equal && (input[i] == input[iterator]);
       iterator++;
     }
-    if (equal && next_is_stop(current_end, stops_len)){
+    if (equal && elem_of(current_end, stops, stops_len)){
       count++;
 
       // delete word not to count it again.
@@ -80,45 +56,30 @@ int compare_words(int begin, int end, int text_len, int stops_len, int index){
     current_begin = stops[stop_iterator] + 1;
   }
   return count;
-
 }
+
 int main () {
-  int input_len = 0;
   cout << "Please enter something: " << endl;
   cin.getline(input, sizeof(input));
 
   // find word separators
-  int ascii;
-  int stops_iterator = 0; // represents # words-1.
+  int input_len = 0;
+  int stops_iter = 0; // represents # words-1.
   stops[0] = 0;
-  for (int i=1;i<SIZE;i++){
-    if (input[i] == '\0') // end of string?
-      break;
+  stops_iter = find_splits_and_count(stops_iter, &input_len);
 
-    // find out how many words we have.
-    ascii = (int) input[i];
-    if (ascii < 65 || ascii > 122 ) {
-      stops[stops_iterator] = i;
-      stops_iterator++;
-    }
-    ++input_len;
-  }
-  // add last stop
-  stops[stops_iterator] = input_len+1;
-  stops_iterator++; // there is one more word than stops
   // count words
-  int current_count = 0;
+  int cur_count = 0;
   int begin;
-  for (int i=0;i<stops_iterator;i++){
+  for (int i=0;i<stops_iter;i++){
     begin = i==0? i:stops[i-1]+1;
-    current_count = compare_words(begin, stops[i], input_len, stops_iterator, i);
+    cur_count = compare_words(begin, stops[i], input_len, stops_iter, i);
     if (input[begin] != '^'){
       cout << "Word '";
       for (int j=begin;j<stops[i];j++)
         cout << input[j];
-      cout << "' is " << current_count << " times in text." << endl;
+      cout << "' is " << cur_count << " times in text." << endl;
     }
   }
-
   return 0;
 }
--- accelerated.h
+++ accelerated.h
@@ -1,7 +1,71 @@
+/*
+        Book: Accelerated C++, Koenig & Moo.
+        @author: Luisa Castano, fernanda16400@gmail.com
+*/
 #include <iostream>
 #include <string>
 #include <cmath>
 
-// 33couter.cc
-bool next_is_stop(int index, int stops_len);
+#define SIZE 100
+
+char input[SIZE];
+int stops[SIZE/2];
+using namespace std;
+
+template <class T>
+void print_array (T *array, int begin, int end){
+  for (int i=begin;i<end;i++)
+    cout << array[i] << " ";
+  cout << endl;
+}
+
+/*********************** 33couter.cc ***************************/
+// checks if 'elem' is an element of 'array'
+bool elem_of(int elem, int* array, int array_len){
+  bool res = true;
+  for (int i=0;i<array_len;i++){
+    res = array[i] == elem;
+    if (res)
+      break;
+  }
+  return res;
+}
+
+/* returns the amount of times that word is repeated in text
+ *
+ * begin:     index of char where word being compared starts with.
+ * end:       index of char where word being compared ends + 1.
+ * text_len:  length of user input.
+ * stops_len: amount of white spaces, equal amout of words.
+ * index:     current stop index.
+ */
 int compare_words(int begin, int end, int text_len, int stops_len, int index);
+
+/* finds out how many not letter chars (stops) there are in input,
+   and counts the words between those ones.
+ *
+ * stops_iter: iterates the stops. Equals amount of counted words - 1.
+ * input_len:  length of user input.
+ */
+int find_splits_and_count(int stops_iter, int* input_len){
+ int ascii;
+ stops[0] = 0;
+ for (int i=1;i<SIZE;i++){
+   if (input[i] == '\0') // end of str?
+     break;
+   // find out how many words we have.
+   ascii = (int) input[i];
+   if (ascii < 65 || ascii > 122 ) {
+     stops[stops_iter] = i;
+     stops_iter++;
+   }
+   ++(*input_len);
+ }
+ // add last stop
+ stops[stops_iter] = (*input_len) + 1;
+ stops_iter++; // there is one more word than stops
+
+ return stops_iter;
+}
+
+/******************** 34long_and_shortest.cc ************************/
